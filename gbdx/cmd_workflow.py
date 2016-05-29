88b9c2f6348a80ead51d95a6f56759c9ee567ca2
Michael Connor <mike@sparkgeo.com>
2016-05-29 16:25:47 -0700

    Update workflow commands.

--- gbdx/cmd_workflow.py
+++ gbdx/cmd_workflow.py
@@ -11,55 +11,93 @@ def workflow():
 # Required for dynamic import
 cli = workflow
 
+
 url_root = '%s/%s' % (host, 'workflows/v1')
 workflows_url = url_root + '/workflows'
 schema_url = url_root + '/schemas/WorkflowDescriptor'
 
 
-@workflow.command('list_tasks')
+@workflow.command('ls')
 @pass_context
-def list_tasks(ctx):
-    """List workflow tasks available to the user"""
-    ctx.show(ctx.gbdx.workflow.list_tasks())
-
-
-@workflow.command('describe_task')
-@click.option('--name','-n', type=click.STRING,
-    help="Name of task to describe")
-@pass_context
-def describe_task(ctx, name):
-    """Show the task description json for the task named"""
-    ctx.show(ctx.gbdx.workflow.describe_task(name))
+def list_workflows(ctx):
+    """Get a list of all workflows."""
+    ctx.show(ctx.get(workflows_url))
 
 
 @workflow.command('status')
-@click.option('--id','-i', type=click.STRING,
-    help="ID of the workflow to status")
-@click.option('--verbose', '-v', is_flag=True, help='Output a more detailed status of workflow')
+@click.argument('id', nargs=-1, type=click.INT)
+@click.option('--verbose', '-v', is_flag=True,
+    help='Output a more detailed status of workflow')
 @pass_context
 def status(ctx, id, verbose):
-    """Display the status information for the workflow ID given"""
+    """Display the status information for the workflow ID(s) given."""
+    id = [str(i) for i in id]
+    is_multistatus = len(id) > 1
     if not verbose:
-        ctx.show(ctx.gbdx.workflow.status(id))
+        if is_multistatus:
+            'Return multistatuses'
+            r = ctx.post(workflows_url + '/multistatus', data=json.dumps(id))
+            ctx.show(build_workflows_status(r.json(), less=True))
+        else:
+            'Return single status'
+            ctx.show(ctx.gbdx.workflow.status(id[0]))
     else:
-        response = ctx.get(workflows_url + '/' + id)
-        r_dict = response.json()
-        ctx.show('Worflow %s\n' % id)
-        tasks = r_dict['tasks']
-        for task in tasks:
-            ctx.show({
-                'name': task['name'],
-                'state': task['state'],
-                'note': task['note']
+        if is_multistatus:
+            'Get multistatuses'
+            response = ctx.post(workflows_url + '/multistatus', data=json.dumps(id))
+            r_dict = response.json()
+        else:
+            'Get single status'
+            response = ctx.get(workflows_url + '/' + id[0])
+            r_dict = [response.json()]
+
+        ctx.show(build_workflows_status(r_dict))
+
+
+
+def build_workflows_status(api_status, less=False):
+    """
+    Build and show the status for the provided workflow(s)
+    :param api_status: a list of workflow statuses, or a single workflow status.
+    """
+
+    if not isinstance(api_status, list):
+        api_status = [api_status]
+
+    output_str = []
+
+    # Show verbose info for workflow and tasks
+    for wf in api_status:
+
+        if less:
+            # Parse workflow detials only
+            output_str.append({'Worflow %s' % wf['id']: wf['state']})
+        else:
+            task_list = []
+            # Parse task details
+            for task in wf['tasks']:
+                task_list.append({
+                    'name': task['name'],
+                    'state': task['state'],
+                    'note': task['note'],
+                    'start time': task['start_time']
+                })
+
+            output_str.append({
+                'Worflow %s' % wf['id']: {
+                    'Tasks': task_list,
+                    'Status': wf['state']
+                }
             })
+
+    return output_str
 
 
 @workflow.command('cancel')
-@click.option('--id','-i', type=click.STRING,
-    help="ID of the workflow to cancel")
+@click.argument('id', nargs=1, type=click.INT)
 @pass_context
 def cancel_workflow(ctx, id):
-    """Cancel the given workflow"""
+    """Cancel the given workflow ID"""
     ctx.show(ctx.gbdx.workflow.cancel(id))
 
 
@@ -74,11 +112,10 @@ def launch_workflow_from_file(ctx, filename):
 
 
 @workflow.command('events')
-@click.option('--id','-i', type=click.STRING,
-    help="ID of the workflow")
+@click.argument('id', nargs=1, type=click.INT)
 @pass_context
 def workflow_events(ctx, id):
-    """Get the given workflows task events."""
+    """Get the given workflow ID task events."""
     ctx.show(ctx.get('%s/%s/%s' %
         (workflows_url, id, 'events')
     ))
@@ -98,10 +135,15 @@ def get_workflow_schema(ctx):
     help='Number of hours to lookback, 720hrs max.')
 @click.option('--owner', '-o', type=click.STRING,
     help='Username of owner. Super user access required.')
-@click.option('--state', '-s', type=click.STRING,
+@click.option('--state', '-s', type=click.Choice([
+        'all', 'submitted', 'scheduled', 'started', 'canceled',
+        'cancelling', 'failed', 'succeeded', 'timedout',
+        'pending','running', 'complete'
+    ]),
     help='Name of the state to filter by.')
 @pass_context
 def search(ctx, schema, lookback_hours, owner, state):
+    """Search workflow database by lookback time, owner, or state."""
     search_url = workflows_url + '/search'
     if schema:
         ctx.show(ctx.get(search_url))
@@ -111,8 +153,10 @@ def search(ctx, schema, lookback_hours, owner, state):
             "owner": owner,
             "state": state
         }
+
+        # Remove keys with None values
         post_data = {k:v for k, v in body.iteritems() if v is not None}
-        print post_data
+
         ctx.show(ctx.post(
             search_url,
             data=json.dumps(post_data)
